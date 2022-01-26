8325f31460160a3e9fe2ce29c5615e4258194243
Jenny Kim <kimj1905@gmail.com>
2022-01-26 15:16:31 -0500

    done

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,10 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+	return [...str].reverse().join("") === str;
+}
+
+palindrome("madam");
 
 module.exports = palindrome;
