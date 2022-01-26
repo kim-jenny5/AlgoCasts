6100896a74311c9c4d97b2d55aa7e7df0f09fd3a
Jenny Kim <kimj1905@gmail.com>
2022-01-26 16:42:29 -0500

    done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,34 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	const obj = {};
+
+	for (const char of str) {
+		obj[char] ? obj[char]++ : (obj[char] = 1);
+	}
+
+	let count = 0;
+	let char = "";
+
+	for (const key in obj) {
+		if (obj[key] > count) {
+			count = obj[key];
+			char = key;
+		}
+		// } else {
+		// 	count;
+		// }
+	}
+
+	// for (const o of Object.entries(obj)) {
+	// 	if (o[1] === count) {
+	// 		return o[0];
+	// 	}
+	// }
+	return char;
+}
+
+maxChar("abcccccccd");
 
 module.exports = maxChar;
