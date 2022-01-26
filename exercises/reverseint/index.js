6629731b8d29b19aabae4c37d43e4fe2bf9d5dac
Jenny Kim <kimj1905@gmail.com>
2022-01-26 15:45:23 -0500

    added another solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,15 +9,21 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
+	// NOTE: SOLUTION 1
 	// let str;
 	// n < 0 ? (str = -n) : (str = n);
 	// const arr = [...str.toString()];
 	// const int = n < 0 ? -arr.reverse().join("") : arr.reverse().join("");
 	// return parseInt(int);
-	const int = n < 0 ? -n : n;
-	const arr = [...int.toString()];
-	const solution = parseInt(arr.reverse().join(""));
-	return n < 0 ? -solution : solution;
+	// NOTE: SOLUTION 2
+	// const int = n < 0 ? -n : n;
+	// const solution = parseInt(arr.reverse().join(""));
+	// return n < 0 ? -solution : solution;
+	// NOTE: SOLUTION 3
+	const num = Math.abs(n);
+	const arr = [...num.toString()];
+	const int = parseInt(arr.reverse().join(""));
+	return n < 0 ? -1 * int : int;
 }
 
 reverseInt(-15);
